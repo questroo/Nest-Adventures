97a4b7b53d2ac82788cb42669a1cd5a29719f33f
Aisle Neral <aisle.neral@hotmail.com>
2020-01-27 18:34:43 -0800

    Added: EnemyController script for basic player tracking PlayerManager script used as a helper for enemy to track player GameManager

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+    public float attackRadius = 3f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if(distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+    }
+
+
+}
