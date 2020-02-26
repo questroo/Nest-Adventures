8ffbcd29f4afff22c3deaa334e93583c7dd4a37d
questroojustin <questroojustin@gmail.com>
2020-02-25 18:44:45 -0800

    -Added the lock on camera to the CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
@@ -22,32 +20,65 @@ public class CameraController : MonoBehaviour
     [Tooltip("Controls The Visability For The Mouse Cursor")]
     public bool turnOffCursor = false;
 
+    [Tooltip("Controls The Visability For The Mouse Cursor")]
+    public bool isBossFollowOn = false;
+
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
-    
+
+    Quaternion newRot;
+    Vector3 cameraToBoss;
+    public Transform bossTransform;
+    public float cameraSwitchSpeed = 270.0f;
+
+    bool isChanging = false;
     float yaw;
     float pitch;
 
     void LateUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            isBossFollowOn = !isBossFollowOn;
+            isChanging = true;
+        }
+
+
         if (turnOffCursor)
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
 
-        if (Input.GetMouseButton(0))
+        if (!isBossFollowOn)
         {
-            yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
-            pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+            if (Input.GetMouseButton(0))
+            {
+                yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+                pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+                pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+            }
+
+
+            if (isChanging)
+            {
+                Debug.Log("Resetting rotation");
+                currentRotation = transform.eulerAngles;
+                pitch = transform.eulerAngles.x;
+                yaw = transform.eulerAngles.y;
+            }
+            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw, 0.0f), ref rotationSmoothVelocity, rotationSmoothTime);
+            transform.eulerAngles = currentRotation;
+        }
+        else 
+        {
+            cameraToBoss = bossTransform.position - transform.position;
+            newRot = Quaternion.LookRotation(cameraToBoss);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, newRot, Time.deltaTime * cameraSwitchSpeed);
+            currentRotation = transform.eulerAngles;
         }
 
-            pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
-
-            currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
-            transform.eulerAngles = currentRotation;
-            transform.position = target.position - transform.forward * distFromTarget;
+        transform.position = target.position - transform.forward * distFromTarget;
+        isChanging = false;
     }
-
-
 }
