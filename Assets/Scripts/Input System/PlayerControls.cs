1565ba61a3bccd56e7ed2cac32af3d022887a27c
Aisle Neral <aisle.neral@hotmail.com>
2020-04-07 16:36:45 -0700

    Added: - Added a Boss test Scene to get started on creating(re-creating) AI behaviours - Implemented a ground plane as well as a boss cube - This boss object has a box collider aswell to test damage later one - Added the boss controller script with a LookAtPlayer function and gizmos

--- Assets/Scripts/Input System/PlayerControls.cs
+++ Assets/Scripts/Input System/PlayerControls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/PlayerControls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input System/PlayerControls.inputactions'
 
 using System;
 using System.Collections;
