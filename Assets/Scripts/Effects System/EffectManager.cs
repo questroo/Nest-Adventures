3762ecf95e03dc489c6842cd09b95aa136a266e7
Jaidon <jaidon.vanherwaarden@gmail.com>
2020-04-08 16:22:37 -0700

    + Added EffectManager Script for objects using particle effects + Added ParticleSpark prefab (simple sparks particle effect) + Added Sparks material for rendering the ParticleSpark effect + Added folder in Prefabs folder for Particle effects

---
+++ Assets/Scripts/Effects System/EffectManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct NamedEffect
+    {
+        public string name;
+        public ParticleSystem effect;
+    }
+    public NamedEffect[] effects;
+
+    Dictionary<string, ParticleSystem> effectDictionary = new Dictionary<string, ParticleSystem>();
+
+    // On Start, the values input in the inspector will be converted from a list into a dictionary for easier lookup.
+    // NOTE: If any number of particles have the same name, only the first will be added.
+    private void Start()
+    {
+        foreach (NamedEffect effect in effects)
+        {
+            if (effectDictionary.ContainsKey(effect.name))
+            {
+                Debug.LogError("[Effect Manager Script] for [" + name + "] is trying to add two effects with the same key! Effect has not been added.");
+            }
+            else
+            {
+                effectDictionary.Add(effect.name, effect.effect);
+            }
+        }
+    }
+
+    public void PlayEffect(string effectName)
+    {
+        ParticleSystem thisEffect;
+        if(effectDictionary.TryGetValue(effectName, out thisEffect))
+        {
+            thisEffect.Play();
+        }
+    }
+}
