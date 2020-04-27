a98248cfedf6a52bf799e603ba85476790689558
Aisle Neral <aisle.neral@hotmail.com>
2020-04-27 11:00:50 -0700

    Reimplemented a couple of premade assets i.e health bar for boss Re added scripts on proper objects Added Lookradius function on the boss controller script Updated so movement so that boss only moves when player is within range Lookradius member is a public variable for designers to edit/play around with

--- Assets/Scripts/AIScripts/BossController.cs
+++ Assets/Scripts/AIScripts/BossController.cs
@@ -8,7 +8,8 @@ public class BossController : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
 
-    public float meleeAttackRadius;
+    private float meleeAttackRadius;
+    public float lookRadius = 5.0f;
 
     void Start()
     {
@@ -20,14 +21,25 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(target != null)
+        if (target != null)
         {
             Movement();
         }
     }
 
+    public Transform GetTarget()
+    {
+        return target;
+    }
+
+    public float GetMeleeRadius()
+    {
+        return meleeAttackRadius;
+    }
+
     public void LookAtPlayer()
     {
+
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f);
@@ -38,13 +50,19 @@ public class BossController : MonoBehaviour
         float distance = Vector3.Distance(target.position, transform.position);
 
         LookAtPlayer();
-        agent.SetDestination(target.position);
-      
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, meleeAttackRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
 }
--- Assets/Scripts/AIScripts/EnemyStat.cs
+++ Assets/Scripts/AIScripts/EnemyStat.cs
@@ -7,6 +7,7 @@ public class EnemyStat : MonoBehaviour
 {
     public float enemyMaxHealth = 100f;
     public float bossDamage = 5f;
+    public HealthBarManager healthBar;
 
     private float enemyCurrentHealth;
 
@@ -14,12 +15,23 @@ public class EnemyStat : MonoBehaviour
     void Start()
     {
         enemyCurrentHealth = enemyMaxHealth;
+        healthBar.SetMaxHealth(enemyMaxHealth);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
     }
 
     public void TakeDamage(float damage)
     {
 
         enemyCurrentHealth -= damage;
+        Debug.Log("Boss takes " + damage + " damage");
+        healthBar.SetHealth(enemyCurrentHealth);
 
         if (enemyCurrentHealth <= 0f)
         {
