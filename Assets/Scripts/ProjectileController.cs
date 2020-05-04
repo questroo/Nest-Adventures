2623cc2c0915a976907d5cb78d7d207276f68aa1
questroo <42195958+questroo@users.noreply.github.com>
2020-05-04 10:08:27 -0700

    -Tried to import the scripts and prefabs from hdrp

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ProjectileController : MonoBehaviour
 {
-    void Start()
+    public PlayerControls controller;
+    public CharacterManager characterManager;
+
+    public GameObject projectile;
+    public Transform projectileSpawnLocation;
+    public float projectileForce = 2.0f;
+    public float projectileLifeTime = 1.0f;
+    private void Awake()
     {
-        
+        controller = new PlayerControls();
+
+        //controller.ActionMap.Attack.performed += ctx => ShootProjectile();
     }
 
-    void Update()
+    public void ShootProjectile()
     {
-        
+        Debug.Log("projectile fire function entered");
+
+        if (characterManager.GetCurrentPlayerTag() == "Bertha")
+        {
+            Debug.Log("projectile fire");
+            Rigidbody projRb = Instantiate(projectile, projectileSpawnLocation.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            projRb.AddForce(transform.forward * projectileForce);
+            StartCoroutine("DestroyProjectile", projRb.gameObject);
+        }
+    }
+    IEnumerator DestroyProjectile(GameObject proj)
+    {
+        yield return new WaitForSeconds(projectileLifeTime);
+        Destroy(proj);
     }
 }
