de93222a031946885c2cfc9bcd5664d33076fea0
Jaidon <jaidon.vanherwaarden@gmail.com>
2020-05-04 12:19:45 -0700

    +Added Conditional Door scripts and prefabs +Added Materials for ease of use =Changed Testing scenes and particles

---
+++ Assets/Scripts/Environment/Door/ConditionalDoor.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DoorState
+{
+    Locked,
+    Opening,
+    Open
+}
+public enum ConditionType
+{
+    PlayerEnter,
+    EnemiesKilled,
+}
+
+public class ConditionalDoor : MonoBehaviour
+{
+    public Vector3 upPosition;
+    public Vector3 downPosition;
+    public float timeToFullyMove = 4.0f;
+
+    float t = 0.0f;
+    DoorState doorState = DoorState.Locked;
+
+    private void Update()
+    {
+        if (doorState == DoorState.Opening)
+        {
+            t += Time.deltaTime;
+            if (t >= timeToFullyMove)
+            {
+                t = timeToFullyMove;
+                doorState = DoorState.Open;
+
+                MoveTrap();
+            }
+            else
+            {
+                MoveTrap();
+            }
+        }
+    }
+
+    void MoveTrap()
+    {
+        transform.localPosition = Vector3.Lerp(upPosition, downPosition, (t / timeToFullyMove));
+    }
+
+    public void UnlockDoor()
+    {
+        if(doorState == DoorState.Locked)
+            doorState = DoorState.Opening;
+    }
+}
