7c7db31d855e195fa2e92cd0649c950f85038441
JustMeGary <jmgfung@gmail.com>
2020-05-31 15:43:27 -0700

    - Added Animation to Boss Arena - Setup some Trap combos

---
+++ Assets/Animation/Boss Arena/BossRoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRoom : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("Active", true);
+        }
+    }
+
+}
