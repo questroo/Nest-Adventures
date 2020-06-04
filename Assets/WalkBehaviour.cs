99d3d5fd485a6f13265d1754ee284dc874a51734
halyosyx <aisle.neral2525@gmail.com>
2020-06-03 18:25:15 -0700

    Implemented Model Added Idle and Walk animation Reimplemented boss scripts

--- Assets/WalkBehaviour.cs
+++ Assets/WalkBehaviour.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class IdleBehaviour : StateMachineBehaviour
+public class WalkBehaviour : StateMachineBehaviour
 {
     BossController boss;
     Rigidbody rigidBody;
     private float dashTime;
-    private float startDashTime = 5.0f;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         boss = animator.GetComponent<BossController>();
         rigidBody = animator.GetComponent<Rigidbody>();
-        dashTime = startDashTime;
 
     }
 
@@ -23,31 +21,26 @@ public class IdleBehaviour : StateMachineBehaviour
         float distance = Vector3.Distance(boss.GetTarget().position, rigidBody.position);
 
         boss.LookAtPlayer();
-        
+
         if (distance <= boss.GetMeleeRadius())
         {
-            animator.SetTrigger("Attack");
-            dashTime = startDashTime;
+            animator.SetTrigger("Idle");
+            boss.StopMovement();
         }
         else if (distance <= boss.lookRadius)
         {
-            animator.SetTrigger("Walk");
-            dashTime = startDashTime;
-        }
-        else if (distance <= boss.dashRadius && distance >= boss.lookRadius && dashTime <= 0.0f)
-        {
-            animator.SetTrigger("Dash");
-            dashTime = startDashTime;
+            boss.Movement();
         }
         else
         {
-            dashTime -= Time.deltaTime;
+            animator.SetTrigger("Idle");
+            boss.StopMovement();
         }
+
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.ResetTrigger("Attack");
-        animator.ResetTrigger("Walk");
+        animator.ResetTrigger("Idle");
     }
 }
