d5dc49069fd34dab7af87a2abb4ca38b201bede1
JustMeGary <jmgfung@gmail.com>
2020-06-17 16:23:41 -0700

    - Fixed some stuff

---
+++ Assets/Assets/Playable Stuff/LevelLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+
+    public GameObject topFloor;
+
+    public GameObject bottomFloor;
+
+
+    void Start()
+    {
+        bottomFloor.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Tanjiro") || other.CompareTag("Bertha"))
+        {
+            topFloor.SetActive(false);
+            bottomFloor.SetActive(true);
+        }
+
+
+    }
+
+}
