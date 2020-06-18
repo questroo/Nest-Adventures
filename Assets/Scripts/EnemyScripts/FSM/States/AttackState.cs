51fa6fc6e80a83df27b61c0de4415cdf37cc34c0
Jaidon <jaidon.vanherwaarden@gmail.com>
2020-06-17 18:06:09 -0700

    + Added ranged enemy character and several scripts supporting AI + FSM, modular states = Modified destructible

---
+++ Assets/Scripts/EnemyScripts/FSM/States/AttackState.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.EnemyScripts.FSM.States
+{
+    [CreateAssetMenu(fileName = "AttackState", menuName = "Finite State Machine/States/AttackState", order = 5)]
+    public class AttackState : AbstractFSMState
+    {
+        float cooldown = 0.0f;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            StateType = FSMStateType.ATTACK;
+        }
+
+        public override bool EnterState()
+        {
+            enteredState = base.EnterState();
+
+            if (enteredState)
+            {
+                Debug.Log("Entered Attack State");
+            }
+
+            navMeshAgent.isStopped = true;
+
+            return enteredState;
+        }
+
+        public override void UpdateState()
+        {
+            if (enteredState)
+            {
+                if (cooldown > 0)
+                {
+                    cooldown -= Time.deltaTime;
+                }
+
+                float distance = Vector3.Distance(navMeshAgent.transform.position, player.transform.position);
+                if (distance <= rangedEnemy.minWeaponRange)
+                {
+                    finiteStateMachine.EnterState(FSMStateType.MOVEAWAY);
+                    return;
+                }
+                else if(distance >= rangedEnemy.maxWeaponRange)
+                {
+                    finiteStateMachine.EnterState(FSMStateType.CHASE);
+                    return;
+                }
+                else
+                {
+                    navMeshAgent.transform.LookAt(player.transform.position);
+
+                    if (cooldown <= 0)
+                    {
+                        Attack();
+                        cooldown = rangedEnemy.attackCooldown;
+                    }
+                }
+            }
+        }
+
+        void Attack()
+        {
+            GameObject arrow = Instantiate(rangedEnemy.projectileWeapon, rangedEnemy.projectileLaunchPosition.position, rangedEnemy.projectileLaunchPosition.rotation);
+            arrow.GetComponent<Rigidbody>().velocity = navMeshAgent.transform.forward * rangedEnemy.projectileLaunchForce;
+        }
+
+        public override bool ExitState()
+        {
+            base.ExitState();
+
+            Debug.Log("Exiting Attack State");
+
+            navMeshAgent.isStopped = false;
+
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/FSM/States/ChaseState.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.EnemyScripts.FSM.States
+{
+    [CreateAssetMenu(fileName = "ChaseState", menuName = "Finite State Machine/States/Chase", order = 3)]
+    public class ChaseState : AbstractFSMState
+    {
+        float cooldownRemaining = 0.0f;
+
+        float distance;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            StateType = FSMStateType.CHASE;
+            player = FindObjectOfType<PlayerStats>().gameObject;
+        }
+
+        public override bool EnterState()
+        {
+            enteredState = base.EnterState();
+
+            if (enteredState)
+            {
+                Debug.Log("Entered Chase State");
+            }
+
+            navMeshAgent.speed += rangedEnemy.chaseStateSpeedModifier;
+
+            return enteredState;
+        }
+
+        public override void UpdateState()
+        {
+            if (enteredState)
+            {
+                float distance;
+                distance = Vector3.Distance(player.transform.position, navMeshAgent.transform.position);
+
+                if (distance >= rangedEnemy.targetLossRange)
+                {
+                    finiteStateMachine.EnterState(FSMStateType.IDLE);
+                    return;
+                }
+                else if (distance >= rangedEnemy.maxWeaponRange)
+                {
+                    Vector3 newPosition = (player.transform.position - navMeshAgent.transform.position).normalized;
+                    newPosition *= (rangedEnemy.maxWeaponRange / 2.0f);
+                    newPosition += navMeshAgent.transform.position;
+                    navMeshAgent.SetDestination(newPosition);
+                }
+                else if (distance >= rangedEnemy.minWeaponRange && distance <= rangedEnemy.maxWeaponRange)
+                {
+                    finiteStateMachine.EnterState(FSMStateType.ATTACK);
+                    return;
+                }
+            }
+        }
+
+        public override bool ExitState()
+        {
+            base.ExitState();
+
+            Debug.Log("Exiting Chase State");
+
+            navMeshAgent.speed -= rangedEnemy.chaseStateSpeedModifier;
+
+            return true;
+        }
+    }
+}
