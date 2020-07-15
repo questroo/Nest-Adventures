16f0163582590043e88a31a78cf002313b511944
Jaidon <jaidon.vanherwaarden@gmail.com>
2020-07-14 19:04:50 -0700

    Small changes to level.

---
+++ Assets/Scripts/HelperScripts/SceneStreamer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class SceneStreamer : MonoBehaviour
+{
+    
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+}
