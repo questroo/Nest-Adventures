a76411b4396abc5cbfc65ab097446df61ee75f21
PC-Craig\craig <craiglacey@gmail.com>
2020-07-21 20:02:44 -0700

    Added GameLoader Scene. Updating BuildSettings to include GameLoader scene. Added GameLoader, AsyncLoader, ServiceLocator, IGameModule, and ObjectPoolManager scripts Setup initial systems flow and game loading to better reflect a proper boot sequence.

---
+++ Assets/Scripts/Systems/GameLoader.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : AsyncLoader
+{
+    public int sceneIndexToLoad = 1;
+    private static int _sceneIndex = 1;
+    private static GameLoader _instance; // The only singleton you should have.
+
+    public List<Component> moduleComponents = new List<Component>();
+
+    protected override void Awake()
+    {
+        Debug.Log("GameLoader Starting");
+
+        // Saftey check
+        if (_instance != null && _instance != this)
+        {
+            Debug.Log("A duplicate instance of the GameLoader was found, and will be ignored. Only one instance is permitted");
+            Destroy(gameObject);
+            return;
+        }
+
+        // Set reference to this instance
+        _instance = this;
+
+        // Make persistent
+        DontDestroyOnLoad(gameObject);
+
+        // Scene Index Check
+        if (sceneIndexToLoad < 0 || sceneIndexToLoad >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log($"Invalid Scene Index {sceneIndexToLoad} ... using default value of {_sceneIndex}");
+        }
+        else
+        {
+            _sceneIndex = sceneIndexToLoad;
+        }
+
+        // Setup System GameObject
+        GameObject systemsGO = new GameObject("[Systems]");
+        Transform systemsParent = systemsGO.transform;
+        DontDestroyOnLoad(systemsGO);
+
+        // Queue up loading routines
+        Enqueue(IntializeCoreSystems(systemsParent), 1);
+        Enqueue(InitializeModularSystems(systemsParent), 2);
+
+        // Set completion callback
+        CallOnComplete(OnComplete);
+    }
+
+    private IEnumerator IntializeCoreSystems(Transform systemsParent)
+    {
+        // Setup Core Systems
+        Debug.Log("Loading Core Systems");
+
+        yield return null;
+    }
+
+    private IEnumerator InitializeModularSystems(Transform systemsParent)
+    {
+        // Setup Additional Systems as needed
+        Debug.Log("Loading Modular Systems");
+
+        foreach(var comp in moduleComponents) 
+        {
+            if (comp is IGameModule)
+            {
+                var module = comp as IGameModule;
+                yield return module.LoadModule();
+            }
+        }
+
+        yield return null;
+    }
+
+    private void OnComplete()
+    {
+        Debug.Log("GameLoader Completed");
+        StartCoroutine(LoadInitialScene(_sceneIndex));
+    }
+
+    private IEnumerator LoadInitialScene(int index)
+    {
+        Debug.Log("GameLoader Starting Scene Load");
+        yield return SceneManager.LoadSceneAsync(index);
+    }
+}
