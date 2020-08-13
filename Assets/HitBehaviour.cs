ac3e37b8ac6ca38011852b06d864f9038195b5a3
halyosyx <aisle.neral2525@gmail.com>
2020-08-12 19:09:22 -0700

    -Accidentally worked on this branch mb- -Updated hit behaviour -Added box collider -Boss is no kinemativ

--- Assets/HitBehaviour.cs
+++ Assets/HitBehaviour.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class HitBehaviour : StateMachineBehaviour
 {
+    BossController boss;
     public int startHitcount;
     private int hitCount = 3;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        boss = animator.GetComponent<BossController>();
     }
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        float distance = Vector3.Distance(boss.GetTarget().position, animator.transform.position);
+
         if (hitCount <= 0)
         {
             animator.SetTrigger("Counter");
+            hitCount = startHitcount;
+        }
+        else if(hitCount <= 0 && distance > boss.meleeAttackRadius)
+        {
+            animator.SetTrigger("Attack1");
             hitCount = startHitcount;
         }
         else
@@ -31,6 +39,8 @@ public class HitBehaviour : StateMachineBehaviour
     {
         animator.ResetTrigger("Counter");
         animator.ResetTrigger("Hit");
+        animator.ResetTrigger("Attack1");
+
         hitCount--;
 
         Debug.Log("Hitcount " + hitCount);
