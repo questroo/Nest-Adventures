672add4809c363b8d7856b75cab08b6381bbf148
Matthew Au Yeung <galaxys2.matthew0621@gmail.com>
2020-12-18 16:23:01 -0800

    [Matthew] - fixed melee enemy not damaging the player. - fixed particles not showing up when the melee enemy attacks.

--- Assets/Scripts/EnemyScripts/HitBox.cs
+++ Assets/Scripts/EnemyScripts/HitBox.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class HitBox : MonoBehaviour
 {
-    //public Transform attackPoint;
+    public Transform attackPoint;
     private EnemyStat enemyStat;
     private PlayerStats ps;
-    //public float attackRange = 0.5f;
+    public float attackRange = 0.5f;
     //public LayerMask playerLayer;
 
-    //[SerializeField]
-    //private ParticleSystem trail_particle;
+    [SerializeField]
+    private ParticleSystem trail_particle;
 
     private void Start()
     {
@@ -21,40 +21,32 @@ public class HitBox : MonoBehaviour
 
     public void Attack()
     {
-    //    trail_particle.Play();
-    //    Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange);
+        trail_particle.Play();
+        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackRange);
 
-    //    if(hitPlayer.Length == 0)
-    //    {
-    //        int i = 0;
-    //    }
+        if (hitPlayer.Length == 0)
+        {
+            int i = 0;
+        }
 
-    //    foreach (Collider player in hitPlayer)
-    //    {
-    //        player.GetComponent<PlayerStats>().TakeDamage(enemyStat.bossDamage);            
-    //    }
-    //    StartCoroutine(TurnOffParticles());
+        foreach (Collider player in hitPlayer)
+        {
+            SoundManager.PlaySound(SoundManager.Sound.player2_get_hit);
+            ps.TakeDamage(enemyStat.bossDamage);
+        }
+        StartCoroutine(TurnOffParticles());
     }
 
-    //IEnumerator TurnOffParticles()
-    //{
-    //    yield return new WaitForSeconds(0.5f);
-    //    trail_particle.Stop();
-    //}
-   
+    IEnumerator TurnOffParticles()
+    {
+        yield return new WaitForSeconds(0.5f);
+        trail_particle.Stop();
+    }
+
     //private void OnDrawGizmosSelected()
     //{
     //    if (attackPoint == null)
     //        return;
     //    Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     //}
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.CompareTag("Player"))
-        {
-            SoundManager.PlaySound(SoundManager.Sound.player2_get_hit);
-            ps.TakeDamage(enemyStat.bossDamage);
-        }
-    }
 }
